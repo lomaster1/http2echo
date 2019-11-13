519f359305cca156850e339b47e9d1e34e7da78c
lomaster2002 <lomaster2002@gmail.com>
2019-11-13 13:36:14 +0300

    add https (http1) server

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const fs = require('fs');
+const https = require('https');
 const http2 = require('http2');
 let server = http2.createServer();
 let secure_server = http2.createSecureServer({
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+});
+let secure_https_server = https.createServer({
     key: fs.readFileSync('key.pem'),
     cert: fs.readFileSync('cert.pem'),
 });
@@ -14,8 +19,27 @@ function on_stream(stream, headers){
     stream.write(`<!DOCTYPE html><html><body><pre>${h}</pre></body></html>`);
     stream.end();
 }
+secure_https_server.on('secureConnection', socket=>{
+   socket.on('data', d=>console.log(''+d));
+});
+secure_https_server.on('request', (req, res)=>{
+   console.log(req.rawHeaders);
+   res.writeHead(200);
+   let h = JSON.stringify(req.headers, null, 2);
+   res.write(`<!DOCTYPE html><html><body><pre>${h}</pre><a href="/">link</a></body></html>`);
+   res.end();
+});
 let port = process.env.PORT||80
 let secure_port = process.env.SECURE_PORT||443;
 server.listen(port, ()=>console.log(`http listening on :${port}`));
-secure_server.listen(secure_port,
-    ()=>console.log(`https listening on :${secure_port}`));
+if (0)
+{
+	secure_server.listen(secure_port,
+	    ()=>console.log(`http2 secure listening on :${secure_port}`));
+}
+else
+{
+	secure_https_server.listen(secure_port,
+	    ()=>console.log(`https listening on :${secure_port}`));
+}
+

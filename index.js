f9fe2dd6efa4f33c4bac288a44d3c533faad55c6
lomaster2002 <lomaster2002@gmail.com>
2019-11-18 15:01:09 +0300

    updates

--- index.js
+++ index.js
@@ -13,10 +13,12 @@ let secure_https_server = https.createServer({
 server.on('stream', on_stream);
 secure_server.on('stream', on_stream);
 function on_stream(stream, headers){
-    console.log('headers', headers)
-    stream.respond({':status': 200, 'content-type': 'text/html'});
+    console.log('headers', headers);
+    console.log('settings', stream.session.remoteSettings);
+    console.log('-----------------------------------------------------');
+    stream.respond({':status': 200, 'content-type': 'text/html', 'set-cookie': 'a=1', 'cache-control': 'max-age=30'}) 
     let h = JSON.stringify(headers, null, 2);
-    stream.write(`<!DOCTYPE html><html><body><pre>${h}</pre></body></html>`);
+    stream.write(`<!DOCTYPE html><html><body><pre>${h}</pre><a href="/foo">link</a></body></html>`);
     stream.end();
 }
 secure_https_server.on('secureConnection', socket=>{
@@ -24,15 +26,15 @@ secure_https_server.on('secureConnection', socket=>{
 });
 secure_https_server.on('request', (req, res)=>{
    console.log(req.rawHeaders);
-   res.writeHead(200);
+   res.writeHead(200, {'set-cookie': 'a=1'});
    let h = JSON.stringify(req.headers, null, 2);
-   res.write(`<!DOCTYPE html><html><body><pre>${h}</pre><a href="/">link</a></body></html>`);
+   res.write(`<!DOCTYPE html><html><body><pre>${h}</pre><a href="/foo">link</a></body></html>`);
    res.end();
 });
 let port = process.env.PORT||80
 let secure_port = process.env.SECURE_PORT||443;
 server.listen(port, ()=>console.log(`http listening on :${port}`));
-if (0)
+if (1)
 {
 	secure_server.listen(secure_port,
 	    ()=>console.log(`http2 secure listening on :${secure_port}`));
